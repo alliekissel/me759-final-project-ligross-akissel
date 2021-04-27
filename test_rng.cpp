8743284dacffded26b7fa6d98894402c48dd2beb
alexandrakissel <akissel@wisc.edu>
2021-04-27 16:48:52 -0500

    Switch to functions filling an array with a sequence and print out a short sequence to test for correct range

--- test_rng.cpp
+++ test_rng.cpp
@@ -3,47 +3,82 @@
 #include <chrono>
 
 // random number generator using rand()
-float gen_rand_0_to_1_test1();
+void gen_rand_0_to_1_test1(float* random_sequence, int n);
 // random number generator using a distribution with time as seed
-float gen_rand_0_to_1_test2();
+void gen_rand_0_to_1_test2(float* random_sequence, int n);
 // random number generator using a distribution with entropy source as seed
-float gen_rand_0_to_1_test3();
+void gen_rand_0_to_1_test3(float* random_sequence, int n);
 // plot collection of sequences seeded differently and plot on a 2D grid to 
 // demonstrate quality of randomness
 void plot_randomness();
 
-float gen_rand_0_to_1_test1() {
-    const float RANGE = 1.0f;
-    float random_val = (float) rand() / (float) RANGE;
-    return random_val;
+void gen_rand_0_to_1_test1(float* random_sequence, int n) {
+    auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed); // make this a different seed so that it can change for each test sequence
+    for(int i = 0; i < n; i++) {
+        random_sequence[i] = (float) rand() / (float) RAND_MAX;
+    }
 }
 
-float gen_rand_0_to_1_test2() {
+void gen_rand_0_to_1_test2(float* random_sequence, int n) {
     auto seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::mt19937 generator(seed);
     const float min = 0.0, max = 1.0;
     std::uniform_real_distribution<float> dist(min, max);
-    float random_val = dist(generator);
-    return random_val;
+    for(int i = 0; i < n; i++) {
+        random_sequence[i] = dist(generator);
+    }
 }
 
-float gen_rand_0_to_1_test3() {
+void gen_rand_0_to_1_test3(float* random_sequence, int n) {
     std::random_device entropy_source;
     std::mt19937 generator(entropy_source());
     const float min = 0.0, max = 1.0;
     std::uniform_real_distribution<float> dist(min, max);
-    float random_val = dist(generator);
-    return random_val;
+    for(int i = 0; i < n; i++) {
+        random_sequence[i] = dist(generator);
+    }
 }
 
 void plot_randomness() {
     
 }
 
-int main(int argc, char* argv[]) {
-    srand(2438298); // make this a different seed so that it can change for each test sequence
-    
-    // pass in loop number to each function so that you don't reseed for each sequence? 
-    // rather than looping in main function
+int main(int argc, char *argv[])
+{
+    int n = 10;
+    // test1
+    float random_sequence1[n];
+    gen_rand_0_to_1_test1(random_sequence1, n);
 
+    std::cout << "Test 1 sequence" << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << random_sequence1[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    // test2
+    float random_sequence2[n];
+    gen_rand_0_to_1_test2(random_sequence2, n);
+
+    std::cout << "Test 2 sequence" << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << random_sequence2[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    // test3
+    float random_sequence3[n];
+    gen_rand_0_to_1_test3(random_sequence3, n);
+
+    std::cout << "Test 3 sequence" << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << random_sequence3[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
