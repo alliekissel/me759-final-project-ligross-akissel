972e0bcbeff6009f365a8d54e9803186a6c2f58d
alexandrakissel <akissel@wisc.edu>
2021-05-02 17:17:39 -0500

    Fix range of interval to be [0,1]

--- rng.cpp
+++ rng.cpp
@@ -6,8 +6,7 @@
 float gen_rand_0_to_1() {
     std::random_device entropy_source;
     std::mt19937 generator(entropy_source());
-    const float min = 0.0, max = 1.0;
-    // still need to fix range
+    const float min = 0.0, max = std::nextafter(1.0, std::numeric_limits<float>::max());
     std::uniform_real_distribution<float> dist(min, max);
     float random_num = dist(generator);
     return random_num;
