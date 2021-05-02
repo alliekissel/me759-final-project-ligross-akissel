0bbf97842e5f2968bf1c885b199951486213c675
alexandrakissel <akissel@wisc.edu>
2021-05-02 18:07:51 -0500

    Delete unnecessary variables

--- rng.h
+++ rng.h
@@ -3,11 +3,6 @@
 
 #include <random>
 
-// unsigned int seed = 8675309;
-// std::mt19937 generator(seed);
-// const float min = 0.0, max = std::nextafter(1.0, std::numeric_limits<float>::max());
-// std::uniform_real_distribution<float> dist(min, max);
-
 // signatures
 float gen_rand_0_to_1();
 
