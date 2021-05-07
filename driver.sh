d7f74a0f6e184c662315a66e27d3e410e19aeda4
Lewis Gross <ligross@euler.wacc.wisc.edu>
2021-05-07 13:18:47 -0500

    todo for serial timing

--- driver.sh
+++ driver.sh
@@ -3,12 +3,12 @@
 #SBATCH -p wacc
 #SBATCH --time=0-00:02:30
 #SBATCH --nodes=1 --cpus-per-task=1
-#SBATCH --output="serial_sim.out"
-#SBATCH --error="serial_sim.err"
+#SBATCH --output="serial_sim_timing.out"
+#SBATCH --error="serial_sim_timing.err"
 
 
 g++ mc_driver.cpp rng.cpp -o simulation
 ./simulation 100 1
-
+# TODO add scaling loop
 # for debugging
 # g++ mc_driver.cpp rng.cpp -o simulation -g 
